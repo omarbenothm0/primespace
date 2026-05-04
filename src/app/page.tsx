21890c8b482b76af2893f1a7b752a5085d21b955
Omar <Omarbenothm0@gmail.com>
2026-05-04 17:09:36 +0200

    move InternationalCard position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <ProductCatalog />
+      <InternationalCard />
       <HowItWorks />
       <PaymentMethods />
-      <InternationalCard />
       <Reviews />
       <FAQ />
       <CTA />
